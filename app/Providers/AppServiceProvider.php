2a1b11e702e6093f3dbd8e313622a851c84274db
Fabry94 <138035586+Fabry94@users.noreply.github.com>
2023-12-19 20:30:05 +0100

    Add files via upload

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()    
+    {
+        if(Schema::hasTable('categories'))
+        {
+            $categories = Category::all();
+            View::share(['categories'=>$categories]);
+        }
+
+        if(Schema::hasTable('tags')){
+            $tags = Tag::all();
+            View::share(['tags' => $tags]);
+        }
+        
+    }
+
+
+    
+
+}
